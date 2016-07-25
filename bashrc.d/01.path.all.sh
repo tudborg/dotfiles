5d2ae4fea2812fb457e2d1ac7869598645e3f6e1
Henrik Tudborg <henrik@tudb.org>
2016-07-25 13:22:14 +0200

    realpath shim and sublime editor set to `subl --wait`

--- bashrc.d/01.path.all.sh
+++ bashrc.d/01.path.all.sh
@@ -1,3 +1,14 @@
+if [ -x /usr/libexec/path_helper ]; then
+    eval `/usr/libexec/path_helper -s`
+fi
+
+if ! which -s realpath; then
+    # hacky realpath shim
+    realpath() {
+        [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+    }
+fi
+
 if [[ -d ~/bin/ ]]; then
     export PATH="$(realpath ~/bin):$PATH"
 fi
--- bashrc.d/editor.all.sh
+++ bashrc.d/editor.all.sh
@@ -1,5 +1,5 @@
 if which subl >/dev/null; then
-    export EDITOR="subl"
+    export EDITOR="subl --wait"
 elif which vim >/dev/null; then
     export EDITOR="vim"
 fi
