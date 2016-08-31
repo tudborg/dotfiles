5462d74535010e3eaaa8380050354f3064e3b752
Henrik Tudborg <henrik@tudb.org>
2016-08-31 13:28:42 +0200

    Updating prompt command

--- bashrc.d/prompt.all.sh
+++ bashrc.d/prompt.all.sh
@@ -1,13 +1,13 @@
 # PS1 comming up:
 #a wrapper function to keep colors from polluting global space
 function __prompt_command () {
-    local EXIT="$?"
+    local LASTEXIT="$?"
     PS1=""
  
-    local R="\[\033[0m\]" #reset
-    local B="\[\033[1m\]" #bold
-    local D="\[\033[2m\]" #dim
-    local U="\[\033[4m\]" #underline
+    local RESET="\[\033[0m\]" #reset
+    local BOLD="\[\033[1m\]" #bold
+    local DIM="\[\033[2m\]" #dim
+    local UNDERLINE="\[\033[4m\]" #underline
     #took the colors _i_ think are nice and defined as vars
     local DEFAULT="\[\033[39m\]"
     local RED="\[\033[91m\]"
@@ -31,19 +31,29 @@ function __prompt_command () {
     # <> you have diverged from upstream
     # = matches upstream
     export GIT_PS1_SHOWUPSTREAM="auto"
- 
-    if [ $EXIT != 0 ]; then
-        local status="${R}${GREY}(${RED}${EXIT}${R}${GREY})${R} "      # Add red if exit code non 0
+
+
+    local r="$RESET"  # reset sequence
+    local p="$GREEN"  # primary color sequence
+    local d="$GREY"   # dim sequence
+
+    local i="$DIM$WHITE"   # info sequence
+    local e="$RED"    # error sequence
+
+    # exit status in dimmed parens and error color number
+    if [ $LASTEXIT != 0 ]; then
+        local status="${r}${d}(${e}${LASTEXIT}${r}${d})${r} "
     else
         local status=""
     fi
 
+    # virtualenv support
     if [[ "$VIRTUAL_ENV" != "" ]]; then
-        local venv=" ${R}${GREY}(${GREEN}${VIRTUAL_ENV##*/}${R}${GREY})${R}"
+        local venv=" ${r}${d}(${p}${VIRTUAL_ENV##*/}${r}${d})${r}"
     else
         local venv=""
     fi
-     
+
     case "$TERM" in
     xterm*|rxvt*)
         # color
@@ -55,13 +65,11 @@ function __prompt_command () {
 
     local gitline=''
     if type -t __git_ps1 > /dev/null; then
-        gitline="\$(__git_ps1 \"${GREY}(${DEFAULT}${D}%s${GREY})\")"
+        gitline="\$(__git_ps1 \"${d}(${i}%s${r}${d})\")"
     fi
 
-    #make that damn PS1
-    #split in lines to easy readability
-    export PS1="${R}${GREY}[\t] ${BLUE}\w ${gitline}\n${status}${BLUE}\$${venv}${BLUE}>${R} "
-    export PS2="${R}${BLUE}||${R} "
+    export PS1="${r}${d}[\t] \u@\h ${p}\w ${gitline}\n${status}${p}\$${venv}${p}>${r} "
+    export PS2="${r}${p} |${r} "
 
 
 }
