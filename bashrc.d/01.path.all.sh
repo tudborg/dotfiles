151401bcb9de8e9b780364c164b987206fe658b4
Henrik Tudborg <henrik@tudb.org>
2017-03-14 10:31:32 +0100

    Updating path stuff from macbook, also tmux changes

--- bashrc.d/01.path.all.sh
+++ bashrc.d/01.path.all.sh
@@ -2,19 +2,15 @@ if [ -x /usr/libexec/path_helper ]; then
     eval `/usr/libexec/path_helper -s`
 fi
 
-if ! which realpath >/dev/null; then
-    # hacky realpath shim
-    realpath() {
-        [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
-    }
-fi
-
 if [[ -d ~/bin/ ]]; then
-    export PATH="$(realpath ~/bin):$PATH"
+    export PATH="$HOME/bin:$PATH"
 fi
 if [[ -d ~/.bin/ ]]; then
-    export PATH="$(realpath ~/.bin):$PATH"
+    export PATH="$HOME/.bin:$PATH"
 fi
 if [[ -d ~/.local/bin/ ]]; then
-    export PATH="$(realpath ~/.local/bin):$PATH"
+    export PATH="$HOME/.local/bin:$PATH"
+fi
+if [[ -d ~/.cargo/bin/ ]]; then
+    export PATH="$HOME/.cargo/bin:$PATH"
 fi
