6af8e7bad54d93b7c2752e763e80b9a0600daea8
Henrik Tudborg <het@keepfocus.dk>
2019-06-14 12:42:05 +0200

    Fixing some bash completion wrt nix and adding nix stuff

---
+++ bashrc.d/docker.darwin.sh
@@ -0,0 +1,4 @@
+# Docker for Mac completions
+if [ -f /Applications/Docker.app/Contents/Resources/etc/docker.bash-completion ]; then
+  . /Applications/Docker.app/Contents/Resources/etc/docker.bash-completion
+fi
---
+++ bashrc.d/nix.all.sh
@@ -0,0 +1,11 @@
+if [[ -d $HOME/.nix-profile ]]; then
+    source $HOME/.nix-profile/etc/profile.d/nix.sh
+fi
+
+# will make bash-completion happy when installed via nix
+export XDG_DATA_DIRS="$HOME/.nix-profile/share:${XDG_DATA_DIRS:-/usr/local/share:/usr/share}"
+
+# bash completions from nix:
+if [[ -d $HOME/.nix-profile/share/bash-completion ]]; then
+    source $HOME/.nix-profile/share/bash-completion/bash_completion
+fi
