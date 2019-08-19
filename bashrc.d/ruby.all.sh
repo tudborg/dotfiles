10f45ebeaba0f5041a561084206f69e6c4c01fff
Henrik Tudborg <henrik@tudb.org>
2019-08-19 22:29:04 +0200

    Cache gem home

--- bashrc.d/ruby.all.sh
+++ bashrc.d/ruby.all.sh
@@ -1,6 +1,10 @@
 if command -v ruby >/dev/null && command -v gem >/dev/null; then
-    # move GEM_HOME into home dir. Global install is messy.
-    export GEM_HOME="$(ruby -r rubygems -e 'puts Gem.user_dir')"
+    if [[ -f /tmp/ruby_gem_home ]]; then
+        export GEM_HOME="$(cat /tmp/ruby_gem_home)"
+    else
+        # move GEM_HOME into home dir. Global install is messy.
+        export GEM_HOME="$(ruby -r rubygems -e 'puts Gem.user_dir')"
+    fi
     # prepend, because osx has some native ruby stuff that we cant really touch
     path_prepend "$GEM_HOME/bin"
 fi
