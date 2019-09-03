32dafafd086b2151963421c9bd410c3bafcb77ed
Henrik Tudborg <henrik@tudb.org>
2019-09-03 15:39:10 +0200

    Some switcheroo with kubectl dotfile

--- bashrc.d/kubectl.all.sh
+++ bashrc.d/kubectl.all.sh
@@ -1,11 +1,20 @@
 if command -v kubectl >/dev/null; then
-    complete -o default -F __start_kubectl k
     function k {
         local BLUE='\033[0;34m'
         local BOLD="\033[1m"
         local CLEAR='\033[0m'
-        local context="$(awk '/^current-context:/{print $2}' ~/.kube/config)"
+        local context="$(awk '/^current-context:/{print $2}' $HOME/.kube/config)"
         printf "${BLUE}${BOLD}$context${CLEAR}\n" >&2
         kubectl "$@"
     }
+    if [[ -d $HOME/.kube ]] && [[ ! -f $HOME/.kube/completion.bash.inc ]]; then
+        kubectl completion bash > $HOME/.kube/completion.bash.inc
+    fi
+    # if the kubectl completion is not loaded, load it:
+    if ! command -v __start_kubectl >/dev/null; then
+        source $HOME/.kube/completion.bash.inc
+    fi
+    if command -v __start_kubectl >/dev/null; then
+        complete -o default -F __start_kubectl k
+    fi
 fi
