67c136342935f3b836ee1714b778474f9d16bb41
Henrik Tudborg <het@keepfocus.dk>
2021-04-12 14:54:49 +0200

    Adding alias pg=pgcli and misc utils to alias

--- bashrc.d/00.aliases.all.sh
+++ bashrc.d/00.aliases.all.sh
@@ -1,5 +1,10 @@
-alias week="date +%GW%U"
 if command -v nvim >/dev/null; then
     alias vim=nvim
 fi
+if command -v pgcli >/dev/null; then
+    alias pg=pgcli
+fi
+
 alias v=vim
+alias week="date +%GW%V" # 2020W42, same week number as ugenr.dk
+alias cal="cal -Nw3sDK"
