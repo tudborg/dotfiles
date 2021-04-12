339f02f09280bf98549b82c8bfc81f6274b1b765
Henrik Tudborg <het@keepfocus.dk>
2021-04-12 14:43:31 +0200

    Adding smux wrapper
    smux is a ssh+tmux shortcut wrapper bash function

---
+++ bashrc.d/smux.all.sh
@@ -0,0 +1,26 @@
+# ssh+tmux=smux
+# ssh in to remote host and attach to existing session or create a new one.
+
+if command -v ssh >/dev/null; then
+  smux () {
+    if [[ "$#" -eq 0 ]]; then
+      echo -e "SSH to <destination> and attach to a tmux session.\nAny argument is passed to ssh.\n\nusage: smux <destination> [...ssh opts]" >&2
+      return 1
+    fi
+    ssh -t ${@} -- "tmux new-session -ADs0"
+  }
+  _smux() 
+  {
+      local cur prev opts
+      COMPREPLY=()
+      cur="${COMP_WORDS[COMP_CWORD]}"
+      prev="${COMP_WORDS[COMP_CWORD-1]}"
+      opts=$(
+        awk '/^host/ && $2 !~ /\*/ {print $2}' ~/.ssh/config &&
+        awk '!/\[/{split($1, a, ",");for(i in a){print a[i]}}' ~/.ssh/known_hosts | sort -u
+      )
+      COMPREPLY=( $(compgen -W "$opts" -- ${cur}) )
+      return 0
+  }
+  complete -F _smux smux
+fi
