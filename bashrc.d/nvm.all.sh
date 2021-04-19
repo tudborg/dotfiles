2258ed6cabfb7853179c27348e1b3a162b95dc5c
Henrik Tudborg <het@keepfocus.dk>
2021-04-19 08:16:38 +0200

    Fixing tabs->spaces

--- bashrc.d/nvm.all.sh
+++ bashrc.d/nvm.all.sh
@@ -3,7 +3,7 @@
 if [[ -d "$HOME/.nvm" ]]; then
     export NVM_DIR="$HOME/.nvm"
     nvm () {
-	echo 'lazy loading nvm...' >&2
+    echo 'lazy loading nvm...' >&2
         unset -f nvm
         # this works on my arch machines with the nvm package installed
         if [[ -f "/usr/share/nvm/init-nvm.sh" ]]; then
@@ -11,9 +11,9 @@ if [[ -d "$HOME/.nvm" ]]; then
         # this works on my mac with nvm installed through brew
         elif [[ -f "/usr/local/opt/nvm/nvm.sh" ]]; then
             source "/usr/local/opt/nvm/nvm.sh"
-	else
-	    echo "nvm doesn't seen to be present." >&2
-	    return 1
+    else
+        echo "nvm doesn't seen to be present." >&2
+        return 1
         fi
         nvm "$@"
     }
