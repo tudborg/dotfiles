1753f5fbf75264307f9945a295a43bc9ddf203b5
Henrik Tudborg <het@keepfocus.dk>
2021-12-13 15:07:54 +0100

    If we have bat installed, use that as previewing tool for fzf

--- bashrc.d/zz.fzf.all.sh
+++ bashrc.d/zz.fzf.all.sh
@@ -7,4 +7,10 @@ if command -v fzf > /dev/null; then
   # Key bindings
   # ------------
   source "${FZF_PREFIX}/shell/key-bindings.bash"
+
+  # If we have bat installed, use bat as default preview fzf
+  if command -v bat >/dev/null; then
+    export FZF_DEFAULT_OPTS="--preview='bat --style=changes,numbers -r 0:\$FZF_PREVIEW_LINES -f {+}'"
+  fi
+
 fi
