e66914fb24b7bc50728dc05510052f21aff48248
Henrik Tudborg <hetu@wisehome.dk>
2023-12-11 10:46:14 +0100

    Prefer `code` to `subl` for EDITOR

--- bashrc.d/editor.all.sh
+++ bashrc.d/editor.all.sh
@@ -1,4 +1,6 @@
-if command -v subl >/dev/null; then
+if command -v code >/dev/null; then
+    export EDITOR="code --wait"
+elif command -v subl >/dev/null; then
     export EDITOR="subl --wait"
 elif command -v vim >/dev/null; then
     export EDITOR="vim"
