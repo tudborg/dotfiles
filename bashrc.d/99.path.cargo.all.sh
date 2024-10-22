9758e7229ba21d1daf63e3c53f35b9e16f8b505d
Henrik Tudborg <henrik@tudb.org>
2024-10-22 19:24:06 +0200

    fixes to dotfiles from new machine

--- bashrc.d/99.path.cargo.all.sh
+++
@@ -1,3 +0,0 @@
-if [[ -d "$HOME/.cargo/bin" ]]; then
-    path_append "$HOME/.cargo/bin"
-fi
--- bashrc.d/ansible.all.sh
+++ bashrc.d/ansible.all.sh
@@ -1,11 +1,11 @@
 if command -v ansible >/dev/null && command -v register-python-argcomplete >/dev/null; then
-  eval $(register-python-argcomplete ansible)
-  eval $(register-python-argcomplete ansible-config)
-  eval $(register-python-argcomplete ansible-console)
-  eval $(register-python-argcomplete ansible-doc)
-  eval $(register-python-argcomplete ansible-galaxy)
-  eval $(register-python-argcomplete ansible-inventory)
-  eval $(register-python-argcomplete ansible-playbook)
-  eval $(register-python-argcomplete ansible-pull)
-  eval $(register-python-argcomplete ansible-vault)
+  eval "$(register-python-argcomplete ansible)"
+  eval "$(register-python-argcomplete ansible-config)"
+  eval "$(register-python-argcomplete ansible-console)"
+  eval "$(register-python-argcomplete ansible-doc)"
+  eval "$(register-python-argcomplete ansible-galaxy)"
+  eval "$(register-python-argcomplete ansible-inventory)"
+  eval "$(register-python-argcomplete ansible-playbook)"
+  eval "$(register-python-argcomplete ansible-pull)"
+  eval "$(register-python-argcomplete ansible-vault)"
 fi
